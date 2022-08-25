2fc492ad1e5edd81f26b2684a8aa13ee82e9239d
dinobrasi <58157848+dinobrasi@users.noreply.github.com>
2022-08-25 10:39:44 -0400

    Folder re-org and cleanup

---
+++ app/webserver.py
@@ -0,0 +1,40 @@
+#sudo python3 /home/tim/Documents/www/webserver.py
+
+from flask import Flask, render_template
+from functions import getPublicIP
+import sys
+import time
+
+#net = "eth0"
+net = "wlan0"
+expected_ip = "192.168.1.241"# waiting for the network at boot. I know there is a setting, but it wasn't working for me.
+waiting = True
+counter = 0
+
+while waiting:
+    public_ip = getPublicIP(net)
+    
+    sys.stdout.write("[" + str(counter) + "] Waiting for IP Address " + expected_ip + ". Getting: " + public_ip + "\r")
+    sys.stdout.flush()
+
+    if expected_ip == public_ip:
+        waiting = False
+    else:
+        counter = counter + 1
+        
+        if counter == 1000:
+            waiting = False 
+    
+    time.sleep(1)# waiting for the network at boot. I know there is a setting, but it wasn't working for me.
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+@app.route('/')
+def index():
+    f = open("/home/tim/Documents/etm/www/index.html", "r")
+    return f.read()
+    #return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=False, host=public_ip, port=80)
