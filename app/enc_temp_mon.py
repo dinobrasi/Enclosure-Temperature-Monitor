dde727f96fe9f96eb645a49b5118ca1ed7aa0a77
dinobrasi <58157848+dinobrasi@users.noreply.github.com>
2022-08-26 22:58:55 -0400

    Code update

--- app/enc_temp_mon.py
+++ app/enc_temp_mon.py
@@ -25,7 +25,29 @@ servo1 = servo.Servo(pca.channels[1], min_pulse=600, max_pulse=2600)
 
 GPIO.setwarnings(False)
 
-current_state = config["off"]
+off = config["off"]
+heat = config["heat"]
+cool = config["cool"]
+current_state = off
+
+vent_side_open = config["vent_side_open"]
+vent_side_closed = config["vent_side_closed"]
+vent_front_open = config["vent_front_open"]
+vent_front_closed = config["vent_front_closed"]
+
+gpio_lcd_switch = config["gpio_lcd_switch"]
+gpio_led_heat = config["gpio_led_heat"]
+gpio_led_cool = config["gpio_led_cool"]
+gpio_led_power = config["gpio_led_power"]
+gpio_led_closed = config["gpio_led_closed"]
+gpio_led_open = config["gpio_led_open"]
+gpio_relay_cool = config["gpio_relay_cool"]
+gpio_relay_heat = config["gpio_relay_heat"]
+gpio_sensor_gate = config["gpio_sensor_gate"]
+
+start_heat = config["start_heat"]
+start_cool = config["start_cool"]
+
 lcd = i2c.CharLCD(config["i2c_expander"],
                   config["address"],
                   port=config["port"],
@@ -46,25 +68,25 @@ def setup():
     #print("    Setup")
     GPIO.setmode(GPIO.BCM)
 
-    GPIO.setup(config["lcd_switch"], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(config["relay_heat"], GPIO.OUT, initial=GPIO.HIGH) # high so they are in their default state when powered up
-    GPIO.setup(config["relay_cool"], GPIO.OUT, initial=GPIO.HIGH) # high so they are in their default state when powered up
-    GPIO.setup(config["led_heat"], GPIO.OUT)
-    GPIO.setup(config["led_cool"], GPIO.OUT)
-    GPIO.setup(config["led_power"], GPIO.OUT)
-    GPIO.setup(config["sensor_gate"], GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(config["gate_led_open"], GPIO.OUT)
-    GPIO.setup(config["gate_led_closed"], GPIO.OUT)
+    GPIO.setup(gpio_lcd_switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(gpio_relay_heat, GPIO.OUT, initial=GPIO.HIGH) # high so they are in their default state when powered up
+    GPIO.setup(gpio_relay_cool, GPIO.OUT, initial=GPIO.HIGH) # high so they are in their default state when powered up
+    GPIO.setup(gpio_led_heat, GPIO.OUT)
+    GPIO.setup(gpio_led_cool, GPIO.OUT)
+    GPIO.setup(gpio_led_power, GPIO.OUT)
+    GPIO.setup(gpio_sensor_gate, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(gpio_led_open, GPIO.OUT)
+    GPIO.setup(gpio_led_closed, GPIO.OUT)
     
     # turn relays and LEDs off
-    GPIO.output(config["relay_heat"], True)
-    GPIO.output(config["relay_cool"], True)
-    GPIO.output(config["led_power"], False)
-    GPIO.output(config["led_heat"], False)
-    GPIO.output(config["led_cool"], False)
+    GPIO.output(gpio_relay_heat, True)
+    GPIO.output(gpio_relay_cool, True)
+    GPIO.output(gpio_led_power, False)
+    GPIO.output(gpio_led_heat, False)
+    GPIO.output(gpio_led_cool, False)
     
-    GPIO.output(config["gate_led_open"], False)
-    GPIO.output(config["gate_led_closed"], False)
+    GPIO.output(gpio_led_open, False)
+    GPIO.output(gpio_led_closed, False)
     
     lcd.write_string('Temp:   F Mode:') # row 1, 6 - 1, 17
     lcd.crlf()
@@ -85,50 +107,50 @@ def setGateFile(state):
 
 def turnOn(which):
     #print("    Relay: Switching on " + which)
-    if which == config["heat"]:
-        if current_state == config["cool"]:
-            turnOff(config["cool"])
+    if which == heat:
+        if current_state == cool:
+            turnOff(cool)
         doClose();
-        GPIO.output(config["relay_heat"], GPIO.LOW)
-        GPIO.output(config["relay_cool"], GPIO.LOW) # need fan to do heat as well
-        GPIO.output(config["led_heat"], True)
+        GPIO.output(gpio_relay_heat, GPIO.LOW)
+        GPIO.output(gpio_relay_cool, GPIO.LOW) # need fan to do heat as well
+        GPIO.output(gpio_led_heat, True)
     else:
-        if current_state == config["heat"]:
-            turnOff(config["heat"])
+        if current_state == heat:
+            turnOff(heat)
         doOpen()
-        GPIO.output(config["relay_cool"], GPIO.LOW)
-        GPIO.output(config["led_cool"], True)
+        GPIO.output(gpio_relay_cool, GPIO.LOW)
+        GPIO.output(gpio_led_cool, True)
 
 def turnOff(which):
     #print("    Relay: Switching off " + which)
-    if which == config["heat"]:
-        GPIO.output(config["relay_heat"], GPIO.HIGH)
-        GPIO.output(config["led_heat"], False)
+    if which == heat:
+        GPIO.output(gpio_relay_heat, GPIO.HIGH)
+        GPIO.output(gpio_led_heat, False)
         doOpen()
     else:
-        GPIO.output(config["relay_cool"], GPIO.HIGH)
-        GPIO.output(config["led_cool"], False)
+        GPIO.output(gpio_relay_cool, GPIO.HIGH)
+        GPIO.output(gpio_led_cool, False)
 
 def doOpen():
     #print("    Vent: Open")
-    servo0.angle = config["vent_side_open"]
-    servo1.angle = config["vent_front_open"]
+    servo0.angle = vent_side_open
+    servo1.angle = vent_front_open
     setDisplay_Vent("Open")
     
 def doClose():
     #print("    Vent: Closed")
-    servo0.angle = config["vent_side_closed"]
-    servo1.angle = config["vent_front_closed"]
+    servo0.angle = vent_side_closed
+    servo1.angle = vent_front_closed
     setDisplay_Vent("Closed")
 
 def doLoop():
-    current_state = config["off"]
+    current_state = off
     lcd_state = config["lcd_state"]
     gate_state_last = ""
     gate_state_this = ""    
-    gate_sensor = config["sensor_gate"]
-    gate_led_open = config["gate_led_open"]
-    gate_led_closed = config["gate_led_closed"]
+    gate_sensor = gpio_sensor_gate
+    gate_led_open = gpio_led_open
+    gate_led_closed = gpio_led_closed
     
     while True:
         try:
@@ -136,21 +158,21 @@ def doLoop():
             if sensor_state == False:
                 GPIO.output(gate_led_open, False)
                 GPIO.output(gate_led_closed, True)
-                gate_state_this = "Closed"
+                gate_state_this = "Closed" # for display
             else:
                 GPIO.output(gate_led_open, True)
                 GPIO.output(gate_led_closed, False)
-                gate_state_this = "Open"   
+                gate_state_this = "Open" # for display
             
             if gate_state_last != gate_state_this:
                 if (gate_state_this == "Open"):
-                    GPIO.output(config["gate_led_open"], True)
-                    GPIO.output(config["gate_led_closed"], False)
-                    setGateFile("open")
+                    GPIO.output(gpio_led_open, True)
+                    GPIO.output(gpio_led_closed, False)
+                    setGateFile("open") # for api
                 else:
-                    GPIO.output(config["gate_led_open"], False)
-                    GPIO.output(config["gate_led_closed"], True)
-                    setGateFile("closed")
+                    GPIO.output(gpio_led_open, False)
+                    GPIO.output(gpio_led_closed, True)
+                    setGateFile("closed") # for api
 
                 setDisplay_Gate(gate_state_this)
             
@@ -165,23 +187,23 @@ def doLoop():
             text = "%0.0f" % fahrenheit
             setDisplay_CurrentTemp(text)
             
-            if fahrenheit > config["start_heat"] and fahrenheit < config["start_cool"]:
-                if current_state != config["off"]:
-                    turnOff(config["heat"])
-                    turnOff(config["cool"])
-                    current_state = config["off"]
+            if fahrenheit > start_heat and fahrenheit < start_cool:
+                if current_state != off:
+                    turnOff(heat)
+                    turnOff(cool)
+                    current_state = off
             else:
-                if fahrenheit <= config["start_heat"]:
-                    if current_state != config["heat"]:
-                        turnOn(config["heat"])
-                        current_state = config["heat"]
+                if fahrenheit <= start_heat:
+                    if current_state != heat:
+                        turnOn(heat)
+                        current_state = heat
                 else:
-                    if fahrenheit >= config["start_cool"]:
-                        if current_state != config["cool"]:
-                            turnOn(config["cool"])
-                            current_state = config["cool"]
+                    if fahrenheit >= start_cool:
+                        if current_state != cool:
+                            turnOn(cool)
+                            current_state = cool
 
-            if GPIO.input(config["lcd_switch"]) == GPIO.LOW:
+            if GPIO.input(gpio_lcd_switch) == GPIO.LOW:
                 if lcd_state == True:
                     lcd_state = False
                 else:
@@ -205,15 +227,15 @@ def setDisplay_CurrentTemp(temp):
 
 def setDisplay_CurrentState(state):
     lcd.cursor_pos = (0,16) # base zero
-    if state == "Off":
+    if state == off:
         state = "Off "
     lcd.write_string(state)
 
 def setDisplay_Triggers():
     lcd.cursor_pos = (1,6) # base zero
-    lcd.write_string(str(config["start_heat"]))
+    lcd.write_string(str(start_heat))
     lcd.cursor_pos = (1,16) # base zero
-    lcd.write_string(str(config["start_cool"]))
+    lcd.write_string(str(start_cool))
 
 def setDisplay_Vent(state):
     lcd.cursor_pos = (2,6) # base zero
@@ -236,7 +258,7 @@ if __name__ == '__main__':
 
     try:
         setup()
-        GPIO.output(config["led_power"], True)
+        GPIO.output(gpio_led_power, True)
         doLoop()
         doCleanup()
         #print("End")
